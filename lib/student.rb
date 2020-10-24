3de7bd3eac75b958cb2e3e1cd7aa08cef1f937cb
Mike Barberry <mikebarberry@protonmail.com>
2020-10-24 01:09:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,4 +31,10 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+
+  def self.create(name, grade)
+    students = Students.new(name, grade)
+    students.save
+    students
+  end 
 end
